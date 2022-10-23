e40564cfbc27e7a5e508bacf3551006556332d2c
Andrew Zaiko <andrewzaiko@gmail.com>
2022-10-23 14:19:27 +0300

    Реализовал bad_smell_long_parameter_list

---
+++ bad_smell_long_parameter_list/constants.py
@@ -0,0 +1,1 @@
+SPEED: int = 1
---
+++ bad_smell_long_parameter_list/main.py
@@ -0,0 +1,30 @@
+from constants import SPEED
+
+
+class Unit:
+    speed = SPEED
+
+    def __init__(self, state: str, x: float, y: float):
+        self.state = state
+        self.x = x
+        self.y = y
+
+    def _get_speed(self):
+        if self.state == 'fly':
+            return self.speed * 1.2
+        elif self.state == 'crawl':
+            return self.speed * 0.5
+        else:
+            raise ValueError('Ни летишь, ни ползешь :-)')
+
+    def move(self, direction):
+        speed = self._get_speed()
+
+        if direction == 'UP':
+            self.field.set_unit(y=self.y + speed, x=self.x, unit=self)
+        elif direction == 'DOWN':
+            self.field.set_unit(y=self.y - speed, x=self.x, unit=self)
+        elif direction == 'LEFT':
+            self.field.set_unit(y=self.y, x=self.x - speed, unit=self)
+        elif direction == 'RIGTH':
+            self.field.set_unit(y=self.y, x=self.x + speed, unit=self)
