c6faa7883cc7291803e7eafb28accad8f1e14bb4
Andrew Zaiko <andrewzaiko@gmail.com>
2022-10-23 19:49:22 +0300

    Реализовал bad_smell_big_class

---
+++ bad_smell_big_class/main.py
@@ -0,0 +1,37 @@
+class Warrior:
+    def attack(self):
+        pass
+
+    def defense(self):
+        pass
+
+    def move(self):
+        pass
+
+class Healer:
+    def defense(self):
+        pass
+
+    def move(self):
+        pass
+
+    def heal(self):
+        pass
+
+class Tree:
+    def defense(self):
+        pass
+
+    def on_fire(self):
+        pass
+
+class Trap:
+    def attack(self):
+        pass
+
+
+if __name__ == '__main__':
+    warrior = Warrior()
+    healer = Healer()
+    tree = Tree()
+    trap = Trap()
